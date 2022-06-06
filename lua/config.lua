20004eed1a1edbde30cfb587f145cddc4439cf91
omkar-mohanty <franzohouser@gmail.com>
2022-06-06 09:57:10 +0530

    added new configs
    Signed-off-by: omkar-mohanty <franzohouser@gmail.com>

---
+++ lua/config.lua
@@ -0,0 +1,8 @@
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+end)
+require'lspconfig'.rust_analyzer.setup({})
+
+
